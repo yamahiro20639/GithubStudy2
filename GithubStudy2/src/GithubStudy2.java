ab68b8eab52d264e6e0d948ff12ebfc932cfa8cf
yamahiro20639 <hiroshi20639@icloud.com>
2024-02-19 18:04:53 +0900

    新規関数を実装

--- GithubStudy2/src/GithubStudy2.java
+++ GithubStudy2/src/GithubStudy2.java
@@ -2,7 +2,10 @@
 public class GithubStudy2 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		int A = 3;
+		int B = 4;
+		System.out.println(A * B);
+		System.out.println("test");
 
 	}
 
